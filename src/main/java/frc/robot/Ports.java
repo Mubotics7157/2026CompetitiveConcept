15438af2ead7e40c35af3dff74c9af60686e05ee
527dj <157839604+527dj@users.noreply.github.com>
2026-02-15 16:19:01 -0800

    Updated motor ids and team number

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -5,19 +5,19 @@ import com.ctre.phoenix6.CANBus;
 public final class Ports {
     // CAN Buses
     public static final CANBus kRoboRioCANBus = new CANBus("rio");
-    public static final CANBus kCANivoreCANBus = new CANBus("main");
+    public static final CANBus kCANivoreCANBus = new CANBus("swerve");
 
     // Talon FX IDs
-    public static final int kIntakePivot = 10;
+    public static final int kIntakePivot = 14;
     public static final int kIntakeRollers = 11;
-    public static final int kFloor = 12;
-    public static final int kFeeder = 13;
-    public static final int kShooterLeft = 14;
-    public static final int kShooterMiddle = 15;
-    public static final int kShooterRight = 16;
-    public static final int kHanger = 18;
+    public static final int kFloor = 19;
+    public static final int kFeeder = 27;
+    public static final int kShooterLeft = 24;
+    public static final int kShooterMiddle = 16;
+    public static final int kShooterRight = 20;
+    public static final int kHanger = 12;
 
     // PWM Ports
-    public static final int kHoodLeftServo = 3;
-    public static final int kHoodRightServo = 4;
+    public static final int kHoodLeftServo = 0;
+    public static final int kHoodRightServo = 1;
 }
